21bf0e57c9fb13ffaf1a7be61cba0714587973f5
luisK1ng <93940144+luisK1ng@users.noreply.github.com>
2022-12-16 23:10:23 -0300

    Add files via upload

---
+++ Main (2).cs
@@ -0,0 +1,24 @@
+Arquivo de solução do Microsoft Visual Studio, formato versão 12.00
+# Visual Studio Versão 16
+VisualStudioVersion =  16.0.32929.386
+MinimumVisualStudioVersion =  10.0.40219.1
+Projeto ( "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}" ) = "Projeto3bim" , "Projeto3bim\Projeto3bim.csproj" , "{7B56E4B4-745E-4440-AFA9-FF4C2ED42821}"
+EndProject
+Global
+	GlobalSection ( SolutionConfigurationPlatforms ) = preSolution
+		Depurar | Qualquer CPU = Depurar| Qualquer CPU
+		Lançamento| Qualquer CPU = Liberar| Qualquer CPU
+	EndGlobalSection
+	GlobalSection ( ProjectConfigurationPlatforms ) = postSolution
+		{7B56E4B4-745E-4440-AFA9-FF4C2ED42821} . Depurar | Qualquer CPU . ActiveCfg = Depurar| Qualquer CPU
+		{7B56E4B4-745E-4440-AFA9-FF4C2ED42821} . Depurar | Qualquer CPU . Construir . 0 = Depurar| Qualquer CPU
+		{7B56E4B4-745E-4440-AFA9-FF4C2ED42821} . Lançamento| Qualquer CPU . ActiveCfg = Liberar| Qualquer CPU
+		{7B56E4B4-745E-4440-AFA9-FF4C2ED42821} . Lançamento| Qualquer CPU . Construir . 0 = Liberar| Qualquer CPU
+	EndGlobalSection
+	GlobalSection ( SolutionProperties ) = preSolution
+		HideSolutionNode =  FALSO
+	EndGlobalSection
+	GlobalSection ( ExtensibilityGlobals ) = postSolution
+		SolutionGuid =  {15F520D8-D000-4A44-966F-404911D5FB9F}
+	EndGlobalSection
+EndGlobal
